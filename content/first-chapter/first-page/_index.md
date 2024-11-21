36a63892efcdd14809ef770cb7d0f244d5c79509
nurdanrseker <nurdanrseker@gmail.com>
2024-11-21 23:01:37 +0100

    Initial commit

---
+++ content/first-chapter/first-page/_index.md
@@ -0,0 +1,5 @@
++++
+date = '2024-11-21T22:52:22+01:00'
+draft = true
+title = 'First Page'
++++
---
+++ public/searchindex.en.js
@@ -0,0 +1,26 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "First-Chapters",
+    "uri": "/first-chapter/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
